07c7e6c53fbe4f9f85329fd3e40cc01f07177506
luckysher <luckyshera95@gmail.com>
2017-04-11 09:24:12 +0530

    added main project fileS

---
+++ chatMessenger/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for chatMessenger project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatMessenger.settings")
+
+application = get_wsgi_application()
