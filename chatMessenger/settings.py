6091434f5530b816e6e11630010f22d3c79c115c
luckysher <luckyshera95@gmail.com>
2017-04-14 18:53:45 +0530

    updated settings for the chatmess

--- chatMessenger/settings.py
+++ chatMessenger/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'common',
+    'dashboard',
+    'security',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,11 @@ WSGI_APPLICATION = 'chatMessenger.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgres_psycopg',
+        'NAME': 'chatmess',
+        'USERNAME': 'chatmess',
+        'PASSWORD': 'chatmess',
+        'HOST': 'localhost',
     }
 }
 
