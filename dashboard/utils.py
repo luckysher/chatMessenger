a3e8719157de30b62f64016a5dad4f6f89292d72
luckysher <luckyshera95@gmail.com>
2017-04-22 12:53:19 +0530

    added utils files for handling utility functions

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -1,8 +1,38 @@
-import logging
+##############################################
+#       Chat messenger utils file            #
+##############################################
 
+import logging
 APP_NAME = 'CHATMESS'
 
-
 def getLogger():
-    logging.basicConfig(format='[%(name)][%(levelname)][%(message)]', level=logging.DEBUG)
+    logging.basicConfig(format='[%(name)s][%(levelname)s] %(message)s', level=logging.DEBUG)
     return logging.getLogger(APP_NAME)
+
+# fun for getting the next url posting
+def getNextUrl(request):
+    nextUrl = None
+    if request.method == 'POST' and 'next' in request.POST:
+        nextUrl = request.POST.get('next')
+    return nextUrl
+
+# fun for getting user id from request
+def getUserId(request):
+    return request.__dict__['_user'].getUserId()
+
+def getuser(request):
+    return request.__dict__['_user'].get_full_name()
+
+def getFirstLastName(full_name):
+
+    first_name = ""
+    last_name = ""
+
+    if full_name.find(' ') > 0:
+        first_name = full_name[: full_name.find(' ')]
+        last_name = full_name[full_name.find(' ')+1 : len(full_name)]
+        print(first_name, " ",  last_name)
+    else:
+        first_name = full_name.strip(' ')
+
+    return first_name, last_name
